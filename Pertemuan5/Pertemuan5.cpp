9376988ea8aea94d6a788fa913e64d8beaf3ca7c
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-26 14:45:19 +0700

    Masukkan Variable

--- Pertemuan5/Pertemuan5.cpp
+++ Pertemuan5/Pertemuan5.cpp
@@ -3,13 +3,6 @@ using namespace std;
 
 int bilangan1, bilangan2
 
-void inputdata()
-{
-    cout << "Masukkan bilangan pertama : ";
-    cin >> bilangan1;
-    cout << "Masukkan bilangan kedua : ";
-    cin >> bilangan2;
-}
 
 
 int main()
