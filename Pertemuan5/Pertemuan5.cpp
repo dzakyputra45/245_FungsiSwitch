d21265bbe02d6e79db9919669e4e57f4fbc9d7bd
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-26 15:25:37 +0700

    switch pilihan

--- Pertemuan5/Pertemuan5.cpp
+++ Pertemuan5/Pertemuan5.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int bilangan1, bilangan2
+int bilangan1, bilangan2;
 
 void inputdata()
 {
@@ -26,25 +26,53 @@ int perkalian(int a, int b)
     return a * b;
 }
 
-int pembagian(int a, int b)
+float pembagian(int a, int b)
 {
     return a / b;
 }
 
-
-
 int main()
 {
-    std::cout << "Hello World!\n";
+    int pilihan;
+
+    do
+    {
+        cout << "Kalkulator Sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "=====================" << endl;
+        cout << "Masukan menu pilihan : ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+            inputData();
+            cout << "Hasil Penjumlahan = " << penjumlahan(bilangan1, bilangan2) << endl;
+            break;
+        case 2:
+            inputData();
+            cout << "Hasil Pengurangan = " << pengurangan(bilangan1, bilangan2) << endl;
+            break;
+        case 3:
+            inputData();
+            cout << "Hasil Perkalian = " << perkalian(bilangan1, bilangan2) << endl;
+            break;
+        case 4:
+            inputData();
+            cout << "Hasil Pembagian = " << pembagian(bilangan1, bilangan2) << endl;
+            break;
+        case 5:
+            break;
+
+        default:
+            cout << "Pilihan tidak ada" << endl;
+            break;
+
+        }
+    } while (pilihan != 5);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
