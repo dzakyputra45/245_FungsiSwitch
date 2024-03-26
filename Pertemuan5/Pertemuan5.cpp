b115f14a2e4cd64bd54a7ee21d567f73ed5d5108
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-26 14:46:39 +0700

    Input Data

--- Pertemuan5/Pertemuan5.cpp
+++ Pertemuan5/Pertemuan5.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int bilangan1, bilangan2
 
+void inputdata()
+{
+    cout << "Masukkan bilangan pertama : ";
+    cin >> bilangan1;
+    cout << "Masukkan bilangan kedua : ";
+    cin >> bilangan2;
+}
+
+
 
 
 int main()
